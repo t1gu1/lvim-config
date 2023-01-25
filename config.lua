6295563712cce4e29bc50ebe8a0a361cda28bcd7
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2023-01-24 20:03:42 -0500

    Fix error with git blame since we use master

--- config.lua
+++ config.lua
@@ -110,12 +110,7 @@ lvim.plugins = {
     end,
   },
   {
-    "f-person/git-blame.nvim",
-    event = "BufRead",
-    config = function()
-      vim.cmd "highlight default link gitblame SpecialComment"
-      vim.g.gitblame_enabled = 0
-    end,
+    "f-person/git-blame.nvim"
   },
   {
     "sindrets/diffview.nvim",
