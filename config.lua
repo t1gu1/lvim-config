201c899821752b0400f89a0588abfcbf09966f44
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2023-02-04 01:49:10 -0500

    Add undo tree and update some packages

--- config.lua
+++ config.lua
@@ -32,6 +32,12 @@ lvim.builtin.which_key.mappings['o'] = {
   "Open the current file directory"
 }
 
+lvim.lsp.buffer_mappings.normal_mode['u'] = nil
+lvim.builtin.which_key.mappings['u'] = {
+  "<cmd>Telescope undo<cr>",
+  "Open undo Tree"
+}
+
 -- Menu of keymappings
 lvim.builtin.which_key.mappings["t"] = {
   name = "Types preview",
@@ -206,7 +212,33 @@ lvim.plugins = {
     "metakirby5/codi.vim",
     -- cmd = "Codi",
   },
+  {
+    "debugloop/telescope-undo.nvim",
+    config = function()
+      require("telescope").load_extension("undo")
+    end,
+  }
 }
 
 lvim.keys.visual_mode["<leader>d"] = "d"
 vim.api.nvim_create_user_command('Test', 'CodiNew javascript', {})
+
+lvim.builtin.telescope.extensions.undo = {
+  use_delta = true,
+  use_custom_command = nil, -- setting this implies `use_delta = false`. Accepted format is: { "bash", "-c", "echo '$DIFF' | delta" }
+  side_by_side = false,
+  diff_context_lines = vim.o.scrolloff,
+  entry_format = "state #$ID, $STAT, $TIME",
+  mappings = {
+    i = {
+      -- IMPORTANT: Note that telescope-undo must be available when telescope is configured if
+      -- you want to replicate these defaults and use the following actions. This means
+      -- installing as a dependency of telescope in it's `requirements` and loading this
+      -- extension from there instead of having the separate plugin definition as outlined
+      -- above.
+      ["<cr>"] = require("telescope-undo.actions").yank_additions,
+      ["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
+      ["<C-cr>"] = require("telescope-undo.actions").restore,
+    },
+  },
+}
